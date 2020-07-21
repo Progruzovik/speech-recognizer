dd836fb289581e839505f4320733c1c1da76bdac
Alexandr Ilyushechkin <progruzovik@gmail.com>
2020-07-21 23:31:09 +0300

    improve performance

--- src/main/kotlin/net/progruzovik/speech/SpeechRecognitionApplication.kt
+++ src/main/kotlin/net/progruzovik/speech/SpeechRecognitionApplication.kt
@@ -2,10 +2,18 @@ package net.progruzovik.speech
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.logging.Level
+import java.util.logging.Logger
 
 @SpringBootApplication
 class SpeechRecognitionApplication
 
 fun main(args: Array<String>) {
+    val recognizerLogger: Logger = Logger.getLogger("default.config")
+    recognizerLogger.level = Level.OFF
+    val recognizerConfig = System.getProperty("java.util.logging.config.file")
+    if (recognizerConfig == null) {
+        System.setProperty("java.util.logging.config.file", "ignoreAllSphinx4LoggingOutput")
+    }
     runApplication<SpeechRecognitionApplication>(*args)
 }
--- src/main/kotlin/net/progruzovik/speech/service/SpeechRecognitionService.kt
+++ src/main/kotlin/net/progruzovik/speech/service/SpeechRecognitionService.kt
@@ -4,23 +4,25 @@ import edu.cmu.sphinx.api.Configuration
 import edu.cmu.sphinx.api.SpeechResult
 import edu.cmu.sphinx.api.StreamSpeechRecognizer
 import org.springframework.stereotype.Service
-import org.springframework.web.multipart.MultipartFile
 import java.io.ByteArrayInputStream
-import java.io.InputStream
 
 @Service
-class SpeechRecognitionService(private val recognizerConfig: Configuration) {
+class SpeechRecognitionService(recognizerConfig: Configuration) {
 
+    private val recognizer = StreamSpeechRecognizer(recognizerConfig)
+
+    @Synchronized
     fun recognizeSpeech(speech: ByteArray): HashSet<String> {
-        val recognizer = StreamSpeechRecognizer(recognizerConfig)
         recognizer.startRecognition(ByteArrayInputStream(speech))
 
         val recognitionResult = HashSet<String>()
         var speechResult: SpeechResult? = recognizer.result
         while (speechResult != null) {
-            speechResult.hypothesis
-                .split(' ')
-                .also { recognitionResult.addAll(it) }
+            val hypothesis = speechResult.hypothesis
+            if (hypothesis.isNotEmpty()) {
+                hypothesis.split(' ')
+                    .also { recognitionResult.addAll(it) }
+            }
             speechResult = recognizer.result
         }
 
